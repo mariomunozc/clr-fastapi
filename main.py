99205248945ba7b5efd58c3b5ed23b4b165bebb7
Mario Muñoz C. <83556392+mariomunozc@users.noreply.github.com>
2024-08-20 12:00:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -35,4 +35,4 @@ app = create_app()
 # Punto de entrada para ejecutar la aplicación con Uvicorn
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app=app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app=app, host="0.0.0.0", port=80, reload=True)
